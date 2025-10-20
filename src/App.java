4822e98607624ae46b1a1e63477a1072913ecb3d
Erika-colla <collaguazobelen8@gmail.com>
2025-10-20 12:55:49 -0500

    feat(ups-001): class App and SortBubble

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int[] numeros = {5, 7, 0, -1, 10, 8};
+
+        SortBubble sort = new SortBubble();
+
+        System.out.println("Array Original : ");
+        sort.printArray(numeros);
+        System.out.println("\nOrdenado Ascendente: ");
+        sort.sortAscendente(numeros);
+        sort.printArray(numeros);
+        System.out.println("\nOrdenado Descendente: ");
+        sort.sortDescendente(numeros);
+        sort.printArray(numeros);
+        
+
+        
     }
 }
--- src/SortBubble.java
+++ src/SortBubble.java
@@ -5,7 +5,57 @@ public class SortBubble {
     }
     
     public void sortAscendente(int[] numeros){
+
+        for (int i = 0; i < numeros.length - 1; i++) {
+            boolean hayCambio = false; //swapped
+            for(int j = 0; j < numeros.length - 1 - i; j++){
+               
+                if(numeros[j] > numeros[j + 1]){
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                    
+                    hayCambio = true;
+                }
+            }
+            if(!hayCambio) {
+                break;
+            }
+        }
+
+    }
+
+    public void sortDescendente(int[] numeros){
+
+        for (int i = 0; i < numeros.length - 1; i++) {
+            boolean hayCambio = false;
+            for (int j = 0; j < numeros.length - 1 - i; j++) {
+                
+                if (numeros[j] < numeros[j + 1]) {
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                    
+                    hayCambio = true;
+                }
+            }
+            if(!hayCambio) {
+                break;
+            }
+            
+        }
+    }
+
+    public void printArray(int[] numeros){
+        System.out.print("[");
         
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i]);
+            if(i<numeros.length-1){
+                System.out.print(",");
+            }
+        }
+        System.out.print("]");
     }
 
 }
